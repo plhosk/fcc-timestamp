3175d834745b1aa527e22be1558f6012a6a61c87
Paul Hoskinson <plhosk@gmail.com>
2016-11-18 23:55:58 -0700

    change indentation

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ const express = require('express')
 const app = express()
 
 app.get('*', (req, res) => {
-    let time = moment(decodeURIComponent(req.url.slice(1)), ['X', 'MMMM D, YYYY'], 'en', true)
-    let unix = null;
-    let natural = null;
-    if (time.isValid()) {
-        unix = time.unix()
-        natural = time.format('MMMM D, YYYY')
-    }
-    
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({
-        unix,
-        natural
-    }))
+  let time = moment(decodeURIComponent(req.url.slice(1)), ['X', 'MMMM D, YYYY'], 'en', true)
+  let unix = null;
+  let natural = null;
+  if (time.isValid()) {
+    unix = time.unix()
+    natural = time.format('MMMM D, YYYY')
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify({
+    unix,
+    natural
+  }))
 })
 
 app.listen(8080)
