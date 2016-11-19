0eddd7353c3162a6709d6e97a1b2702f03b43d69
Paul Hoskinson <plhosk@gmail.com>
2016-11-19 03:05:25 +0000

    basic api completed

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 'use strict'
+const moment = require('moment')
+moment().format()
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('*', function (req, res) {
+    let time = moment(decodeURIComponent(req.url.slice(1)), ['X', 'MMMM D, YYYY'], 'en', true)
+    let unix = null;
+    let natural = null;
+    if (time.isValid()) {
+        unix = time.unix()
+        natural = time.format('MMMM D, YYYY')
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({
+        unix,
+        natural
+    }))
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(8080)
